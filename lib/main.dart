f6978927ce252b6a8529857062dbcb6451950a84
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-04 00:31:57 -0300

    x2

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,122 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(home: Scaffold(
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Text('testedasdasdasdasdasdasdasdasdas'),
-              Text('testedasdasdasd'),
-              Text('test'),
-              Text('test'),
-              Text('test'),
-              Text('test'),
-              Text('test'),
-            ],
-          ),
-        ),
-        appBar: AppBar(
-          title: Text('Transfers'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      )),
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: TransferForm(),
+      ),
     );
+  }
+}
+
+class TransferForm extends StatelessWidget {
+  final TextEditingController _accountNumberFieldController = TextEditingController();
+  final TextEditingController _valueFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Creating Transfer'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _accountNumberFieldController,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Account number',
+                  hintText: '0000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _valueFieldController,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Value',
+                  hintText: '0.00'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            child: Text('Confirm'),
+            onPressed: () {
+              debugPrint('clicked on confirm');
+              final int accountNumber = int.tryParse(_accountNumberFieldController.text);
+              final double value = double.tryParse(_valueFieldController.text);
+              if(accountNumber != null && value != null) {
+                final transferCreated = Transfer(value, accountNumber);
+                debugPrint('$transferCreated');
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TransfersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transfers'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TransferItem(Transfer(100.0, 1000)),
+          TransferItem(Transfer(200.0, 2000)),
+          TransferItem(Transfer(300.0, 3001)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transfer.value.toString()),
+        subtitle: Text(_transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+
+  @override
+  String toString() {
+    return 'Transfer{value: $value, accountNumber: $accountNumber}';
+  }
+}
