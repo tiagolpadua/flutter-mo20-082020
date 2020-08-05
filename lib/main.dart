87dff464163a0ae9ff3e93d73936affea72ce87c
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-05 07:34:54 -0300

    ex

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,64 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.green[900],
-        accentColor: Colors.blueAccent[700],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary,
-        ),
-      ),
-      home: TransfersList(),
+      theme: bytebankTheme(),
+      home: Dashboard(),
     );
   }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              height: 100,
+              width: 150,
+              color: Theme.of(context).primaryColor,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.people, color: Colors.white),
+                    Text('Contacts', style: TextStyle(color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+ThemeData bytebankTheme() {
+  return ThemeData(
+    primaryColor: Colors.green[900],
+    accentColor: Colors.blueAccent[700],
+    buttonTheme: ButtonThemeData(
+      buttonColor: Colors.blueAccent[700],
+      textTheme: ButtonTextTheme.primary,
+    ),
+  );
 }
