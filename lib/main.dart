9f718f09ad0df51b85ba61341300e7eb14c70119
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-06 00:29:50 -0300

    exc7

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:bytebank/screens/transfers/list.dart';
+import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/screens/contacts_list.dart';
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BytebankApp());
@@ -9,51 +11,6 @@ class BytebankApp extends StatelessWidget {
     return MaterialApp(
       theme: bytebankTheme(),
       home: Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  const Dashboard({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(Icons.people, color: Colors.white),
-                    Text('Contacts', style: TextStyle(color: Colors.white)),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'contact_form.dart';
+
+class ContactsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('Alex'),
+              subtitle: Text('1000'),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ContactForm(),
+            ),
+          ).then((newContact) => debugPrint(newContact.toString()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
