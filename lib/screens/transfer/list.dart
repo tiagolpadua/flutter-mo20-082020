239b6b9ace0493a3a6e92cc0ddfaf268fe27976e
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-06 21:35:31 -0300

    ex

--- lib/screens/transfer/list.dart
+++ lib/screens/transfer/list.dart
@@ -1,6 +1,8 @@
 import 'package:bytebank/models/transfer.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
+import '../../main.dart';
 import 'file:///C:/git/mo20-082020/bytebank/lib/screens/transfer/form.dart';
 
 class TransfersList extends StatefulWidget {
@@ -17,44 +19,48 @@ class TransfersListState extends State<TransfersList> {
   Widget build(BuildContext context) {
     const _appBarTitle = 'Transfers';
 
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: widget._transfers.length,
-        itemBuilder: (context, index) {
-          final transfer = widget._transfers[index];
-          return TransferItem(transfer);
-        },
-      ),
-      appBar: AppBar(
-        title: Text(_appBarTitle),
-        actions: <Widget>[
-          // action button
-          IconButton(
-            icon: Icon(Icons.lightbulb_outline),
-            onPressed: () {
-              debugPrint('lightbulb pressed!');
+    return ScopedModelDescendant<DarkModeModel>(
+      builder: (context, child, model) {
+        return Scaffold(
+          body: ListView.builder(
+            itemCount: widget._transfers.length,
+            itemBuilder: (context, index) {
+              final transfer = widget._transfers[index];
+              return TransferItem(transfer);
             },
           ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          final Future future =
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return TransferForm();
-          }));
-          future.then((transferReceived) {
-            debugPrint('arrived at then of the future');
-            debugPrint('$transferReceived');
-            if (transferReceived != null) {
-              setState(() {
-                widget._transfers.add(transferReceived);
+          appBar: AppBar(
+            title: Text(_appBarTitle),
+            actions: <Widget>[
+              // action button
+              IconButton(
+                icon: Icon(Icons.lightbulb_outline),
+                onPressed: () {
+                  model.toggle();
+                },
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              final Future future =
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return TransferForm();
+              }));
+              future.then((transferReceived) {
+                debugPrint('arrived at then of the future');
+                debugPrint('$transferReceived');
+                if (transferReceived != null) {
+                  setState(() {
+                    widget._transfers.add(transferReceived);
+                  });
+                }
               });
-            }
-          });
-        },
-      ),
+            },
+          ),
+        );
+      }
     );
   }
 }
