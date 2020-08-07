5230240d227499d87700ebc4dd47ab8bebf96ff3
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-06 22:18:41 -0300

    ex

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bytebank/screens/transfers/list.dart';
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BytebankApp());
@@ -28,29 +29,48 @@ class Dashboard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/bytebank_logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(Icons.people, color: Colors.white),
-                    Text('Contacts', style: TextStyle(color: Colors.white)),
-                  ],
+          FutureBuilder<double>(
+            initialData: 0.2,
+            future: Future.delayed(Duration(microseconds: 100)).then((value) => 1),
+            builder: (context, snapshot) {
+              debugPrint('${snapshot.data}');
+              return AnimatedOpacity(
+                opacity: snapshot.data,
+                curve: Curves.easeInCubic,
+                duration: Duration(seconds: 2),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset('images/bytebank_logo.png'),
                 ),
-              ),
-            ),
+              );
+            }
+          ),
+          FutureBuilder<double>(
+            initialData: 0.7,
+            future: Future.delayed(Duration(microseconds: 100)).then((value) => 1),
+            builder: (context, snapshot) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: AnimatedContainer(
+                  duration: Duration(seconds: 2),
+                  padding: EdgeInsets.all(8.0),
+                  height: 100 * snapshot.data,
+                  width: 150 * snapshot.data,
+                  color: Theme.of(context).primaryColor,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Icon(Icons.people, color: Colors.white),
+                        Text('Contacts', style: TextStyle(color: Colors.white)),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }
           ),
         ],
       ),
