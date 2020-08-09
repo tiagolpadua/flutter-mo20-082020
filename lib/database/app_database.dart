9fb31d6e504da98638814bb0000ebbf975d57e96
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-09 20:06:53 -0300

    ex

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -3,15 +3,20 @@ import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-Future<Database> createDatabase(){
+Future<Database> createDatabase() {
   return getDatabasesPath().then((dbPath) {
     final String path = join(dbPath, 'bytebank.db');
-    return openDatabase(path, onCreate: (db, version){
-      db.execute('CREATE TABLE contacts('
-          'id INTEGER PRIMARY KEY, '
-          'name TEXT, '
-          'account_number INTEGER)');
-    }, version: 1);
+    return openDatabase(
+      path,
+      onCreate: (db, version) {
+        db.execute('CREATE TABLE contacts('
+            'id INTEGER PRIMARY KEY, '
+            'name TEXT, '
+            'account_number INTEGER)');
+      },
+      version: 1,
+      // onDowngrade: onDatabaseDowngradeDelete,
+    );
   });
 }
 
