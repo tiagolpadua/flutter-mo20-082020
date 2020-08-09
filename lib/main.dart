7d1df8e0e69563faacd7282258bfb109d954ba09
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-09 19:15:07 -0300

    ex

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'database/app_database.dart';
-import 'file:///C:/git/mo20-082020/bytebank/lib/screens/contacts/form.dart';
-import 'file:///C:/git/mo20-082020/bytebank/lib/screens/contacts/list.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
-
-import 'models/contact.dart';
 
 void main() {
   runApp(BytebankApp());
