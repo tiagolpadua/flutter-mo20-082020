13e562e4795b356c2baa347f672d5029ab115d9f
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-10 07:59:55 -0300

    ex

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,6 +1,29 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+String ip = '192.168.0.100';
 
 void findAll() async {
-  final Response response = await get('http://192.168.0.100:8080/transactions');
+  Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response = await client.get('http://$ip:8080/transactions');
   print(response.body);
+}
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('url ${data.url}');
+    print('headers ${data.headers}');
+    print('body ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
 }
