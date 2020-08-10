a514a53c18ac6fe645467d833a2ad67fbb230a1e
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2020-08-09 22:22:19 -0300

    ex

--- lib/screens/contacts/list.dart
+++ lib/screens/contacts/list.dart
@@ -17,7 +17,7 @@ class _ContactsListState extends State<ContactsList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contacts'),
+        title: Text('Transfer'),
       ),
       body: FutureBuilder<List<Contact>>(
         initialData: List(),
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'contacts/list.dart';
 
 class Dashboard extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,35 +17,80 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    (MaterialPageRoute(
-                      builder: (context) => ContactsList(),
-                    )),
-                  );
+          Row(
+            children: [
+              _FeatureItem(
+                'Transfer',
+                Icons.monetization_on,
+                onClick: () {
+                  _showContactsList(context);
                 },
-                child: Container(
-                  padding: EdgeInsets.all(8.0),
-                  height: 100,
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(Icons.people, color: Colors.white),
-                      Text('Contacts', style: TextStyle(color: Colors.white)),
-                    ],
-                  ),
-                ),
               ),
-            ),
+              _FeatureItem(
+                'Transaction Feed',
+                Icons.description,
+                onClick: () => print('transaction feed was clicked'),
+              ),
+            ],
           ),
         ],
+      ),
+    );
+  }
+
+  void _showContactsList(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ContactsList(),
+      ),
+    );
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  _FeatureItem(this.name, this.icon, {@required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+//          onTap: () => Navigator.of(context).push(
+//              (MaterialPageRoute(
+//                builder: (context) => ContactsList(),
+//              )),
+//            ),
+          child: Container(
+            padding: EdgeInsets.all(8.0),
+            height: 100,
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 24.0,
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
