6baaec1902d2956acd35c32e5759ee3eadb3e48a
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-08-11 08:27:13 -0300

    transfer list from webapi

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -11,10 +11,12 @@ Future<List<Transaction>> findAll() async {
   final Client client = HttpClientWithInterceptor.build(
     interceptors: [LoggingInterceptor()],
   );
-  final Response response = await client.get('http://$ip:8080/transactions');
+  final Response response = await client
+      .get('http://$ip:8080/transactions')
+      .timeout(Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = List();
-  for(Map<String, dynamic> transactionJson in decodedJson){
+  for (Map<String, dynamic> transactionJson in decodedJson) {
     final Map<String, dynamic> contactJson = transactionJson['contact'];
     final Transaction transaction = Transaction(
       transactionJson['value'],
--- lib/screens/transactions/list.dart
+++ lib/screens/transactions/list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/centered_message.dart';
 import 'package:bytebank/components/progress.dart';
 import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/models/transaction.dart';
@@ -25,34 +26,42 @@ class TransactionsList extends StatelessWidget {
             case ConnectionState.active:
               break;
             case ConnectionState.done:
-              final List<Transaction> transactions = snapshot.data;
-              return ListView.builder(
-                itemBuilder: (context, index) {
-                  final Transaction transaction = transactions[index];
-                  return Card(
-                    child: ListTile(
-                      leading: Icon(Icons.monetization_on),
-                      title: Text(
-                        transaction.value.toString(),
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
+              if (snapshot.hasData) {
+                final List<Transaction> transactions = snapshot.data;
+                if (transactions.isNotEmpty) {
+                  return ListView.builder(
+                    itemBuilder: (context, index) {
+                      final Transaction transaction = transactions[index];
+                      return Card(
+                        child: ListTile(
+                          leading: Icon(Icons.monetization_on),
+                          title: Text(
+                            transaction.value.toString(),
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          subtitle: Text(
+                            transaction.contact.accountNumber.toString(),
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
                         ),
-                      ),
-                      subtitle: Text(
-                        transaction.contact.accountNumber.toString(),
-                        style: TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ),
-                    ),
+                      );
+                    },
+                    itemCount: transactions.length,
                   );
-                },
-                itemCount: transactions.length,
+                }
+              }
+              return CenteredMessage(
+                'No transactions found',
+                icon: Icons.warning,
               );
               break;
           }
-          return Text('Unknown error');
+          return CenteredMessage('Unkown error');
         },
       ),
     );
