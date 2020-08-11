9e8536e4a09aae7cba2b9b0ddcdc0c6e6794b05a
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-08-11 07:55:09 -0300

    transfer list from webapi

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,12 +1,32 @@
+import 'dart:convert';
+
+import 'package:bytebank/models/contact.dart';
+import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
 String ip = '192.168.0.100';
 
-void findAll() async {
-  Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+Future<List<Transaction>> findAll() async {
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+  );
   final Response response = await client.get('http://$ip:8080/transactions');
-  print(response.body);
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = List();
+  for(Map<String, dynamic> transactionJson in decodedJson){
+    final Map<String, dynamic> contactJson = transactionJson['contact'];
+    final Transaction transaction = Transaction(
+      transactionJson['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
+    transactions.add(transaction);
+  }
+  return transactions;
 }
 
 class LoggingInterceptor implements InterceptorContract {
