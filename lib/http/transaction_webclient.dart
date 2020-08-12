dab36ecfb5420567efee0481fe82bb7c9cadabd3
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-08-12 08:16:19 -0300

    c15

--- lib/http/transaction_webclient.dart
+++ lib/http/transaction_webclient.dart
@@ -9,10 +9,12 @@ import 'package:http/http.dart';
 
 class TransactionWebClient {
   Future<List<Transaction>> findAll() async {
-    final Response response = await client
-        .get('http://$ip:8080/transactions')
-        .timeout(Duration(seconds: 5));
-    return _toTransactions(response);
+    final Response response =
+    await client.get(baseUrl).timeout(Duration(seconds: 5));
+    final List<dynamic> decodedJson = jsonDecode(response.body);
+    return decodedJson
+        .map((dynamic json) => Transaction.fromJson(json))
+        .toList();
   }
 
   Future<Transaction> save(Transaction transaction) async {
@@ -23,28 +25,6 @@ class TransactionWebClient {
       'password' : '1000'
     }, body: transactionJson);
 
-    return _toTransaction(response);
-  }
-
-  Transaction _toTransaction(Response response) {
-    Map<String, dynamic> json = jsonDecode(response.body);
-    final Map<String, dynamic> contactJson = json['contact'];
-    return Transaction(
-      json['value'],
-      Contact(
-        0,
-        contactJson['name'],
-        contactJson['accountNumber'],
-      ),
-    );
-  }
-
-  List<Transaction> _toTransactions(Response response) {
-    final List<dynamic> decodedJson = jsonDecode(response.body);
-    final List<Transaction> transactions = List();
-    for (Map<String, dynamic> transactionJson in decodedJson) {
-      transactions.add(Transaction.fromJson(transactionJson));
-    }
-    return transactions;
+    return Transaction.fromJson(jsonDecode(response.body));
   }
 }
