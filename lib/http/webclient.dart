a291071ed2f43c10255306e9090c508ce5252bc0
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-08-11 23:26:33 -0300

    c14

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -2,19 +2,25 @@ import 'dart:convert';
 
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/models/transaction.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
-String ip = '192.168.0.100';
+const String ip = '192.168.0.100';
+
+final Client client = HttpClientWithInterceptor.build(
+  interceptors: [LoggingInterceptor()],
+);
+
+const String baseUrl = 'http://$ip:8080/transactions';
 
 Future<List<Transaction>> findAll() async {
-  final Client client = HttpClientWithInterceptor.build(
-    interceptors: [LoggingInterceptor()],
-  );
+  debugPrint('findAll');
   final Response response = await client
       .get('http://$ip:8080/transactions')
       .timeout(Duration(seconds: 5));
   final List<dynamic> decodedJson = jsonDecode(response.body);
+  debugPrint('findAll: $decodedJson');
   final List<Transaction> transactions = List();
   for (Map<String, dynamic> transactionJson in decodedJson) {
     final Map<String, dynamic> contactJson = transactionJson['contact'];
@@ -29,6 +35,34 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber
+    }
+  };
+
+  final String transactionJson = jsonEncode(transactionMap);
+
+  final Response response = await client.post(baseUrl, headers: {
+    'Content-type' : 'application/json',
+    'password' : '1000'
+  }, body: transactionJson);
+
+  Map<String, dynamic> json = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = json['contact'];
+  return Transaction(
+    json['value'],
+    Contact(
+      0,
+      contactJson['name'],
+      contactJson['accountNumber'],
+    ),
+  );
 }
 
 class LoggingInterceptor implements InterceptorContract {
