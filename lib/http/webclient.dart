68e2a5341beb2c3572ca13de659548f768d09336
F9342853 Tiago Lage Payne de Padua <tiagolpadua@bb.com.br>
2020-08-13 10:55:37 -0300

    d0

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -1,15 +1,36 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_client_with_interceptor.dart';
 
-// 1 - Add http dependency to pubscpec.yaml
-// 2 - Create folder http inside lib
-// 3 - Create webclient.dart and implement findAll()
-// 4 - Call findAll in application startup
-// 5 - Why is not working?
-// 6 - Find you IP address or use 10.0.2.2
+import 'package:http_interceptor/interceptor_contract.dart';
+import 'package:http_interceptor/models/request_data.dart';
+import 'package:http_interceptor/models/response_data.dart';
+
+// 1 - add http_interceptor to your pubspec.yaml
+// 2 - Implement LoggingInterceptor a subclass of InterceptorContract
+// 3 - Use HttpClientWithInterceptor to build your client
+// 4 - Test everything
 
 void findAll() async {
-  print('>>>>>>>>>>>>>>>> findAll');
-  final Response response = await get('http://192.168.0.100:8080/transactions');
-  print('>>>>>>>>>>>>>>>> response received');
-  print(response);
+  Client client = HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+  final Response response = await client.get('http://192.168.0.100:8080/transactions');
+}
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('>>>>>>>>>>>> sending request');
+    print('url ${data.url}');
+    print('headers ${data.headers}');
+    print('body ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('>>>>>>>>>>>> receiving response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
 }
